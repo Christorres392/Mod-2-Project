90f73741d38d7231a4b0c850a0b4552c4b1cab41
Gabe-Corral <78gabe.corral@gmail.com>
2020-08-18 18:03:02 -0500

    added code for destinations

---
+++ app/controllers/destinations_controller.rb
@@ -0,0 +1,20 @@
+class DestinationsController < ApplicationController
+
+  def index
+    @destinations = Destination.all
+  end
+
+  def new
+    @destination = Destination.new
+  end
+
+  def create
+    @destination = Destination.create(destination_params)
+  end
+
+  private
+  def destination_params
+    params.require(:destination).permit(:location, :description)
+  end
+
+end
